18b94367d39ad4c061bf1bd3238ce9376bb9e275
KHADIJA SABIR <awaissabir107@gmail.com>
2025-08-19 13:35:47 +0500

    putting comments

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,4 +1,4 @@
-import random
+import random  # Import random module to generate a random number
 
 print("To quit the game type: 'exit'")
 top_of_range=input("Type a number : ") 
@@ -13,15 +13,17 @@ else:
     print("Please type a number.") 
     quit()       
 
+# Choose a number from the range 0 to the top_of_range(provided by user.)
 random_number = random.randint(0,top_of_range)
 
 attempt=0
 
+# Keep guessing the number until you got the right answer
 while True:
     attempt += 1
     user_guess = input("Guess a number : ")
     if user_guess!="exit":
-        if user_guess.isdigit():
+        if user_guess.isdigit():   # Check if the user entered a digit or a text
             user_guess=int(user_guess)
         else:
             print("Please type a number next time.")
@@ -29,13 +31,14 @@ while True:
 
         if user_guess==random_number:
             print("Congratulations! You guessed it right.")
-            break
+            break   
         elif user_guess > random_number:
-            print("You were above the number!")
+            print("You were above the number!")   # Hints for the user
         else:
             print("You were below the number!")
     else:
         quit()                
 
+# Print your score
 print(f"You got it in {attempt} attempts.")
     
